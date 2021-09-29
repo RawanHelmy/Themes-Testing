5fcd2712e4f9e3b9283f8ed7c1e00737ee078b7b
RawanHelmy <rawanhelmy98@gmail.com>
2021-09-29 16:42:17 +0200

    testing components

--- angular-themeing-5qlive/src/app/app.module.ts
+++ angular-themeing-5qlive/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ThemeModule, lightTheme, darkTheme } from './theme';
 import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   imports:      [
@@ -13,7 +14,7 @@ import { TestComponent } from './test/test.component';
       active: 'light'
     })
   ],
-  declarations: [ AppComponent, TestComponent ],
+  declarations: [ AppComponent, TestComponent, Test2Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
